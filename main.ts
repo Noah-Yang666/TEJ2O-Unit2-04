e62e204f6e04c9c5bcad20ad8bd21197f3607084
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-02-20 13:16:25 -0500

    programmed current temperature

--- main.ts
+++ main.ts
@@ -1,8 +1,22 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Noah Yang
+ * Created on: Feb 2026
+ * This program tells the current temperature
 */
 
-basic.showString('Hello, World!')
+// The Variable For Temperature
+let currentTemperature: number
+
+// Preparation
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+
+// The Procedure
+input.onButtonPressed(Button.A, function () {
+  currentTemperature = input.temperature()
+  basic.showString('The temperature is ' + currentTemperature)
+  basic.clearScreen()
+  basic.showIcon(IconNames.Happy)
+})
