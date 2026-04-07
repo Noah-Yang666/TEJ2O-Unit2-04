f969f2dfe0991d69fdd74e883a37a9bc6c8bdeb4
Noah-Yang666 <noah.yang@stu.ocsb.ca>
2026-04-07 17:06:09 +0000

    wrote code

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -6,4 +6,28 @@ This module is a Micro:bit MicroPython program
 
 from microbit import *
 
-display.scroll("Hello, World!")
+
+# setup before a is pressed
+
+display.clear()
+
+display.show(Image.HAPPY)
+
+sleep(1000)
+
+
+# calculates temp then shows it
+
+while True:
+
+    if button_a.was_pressed():
+
+        temperature = temperature()
+
+        display.scroll("The temperature is " + (temperature))
+
+        display.clear()
+
+        display.show(Image.HAPPY)
+
+        sleep(1000)
